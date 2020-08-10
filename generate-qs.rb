01f2c625785a8c216595b681d1e767194ec6b020
Tony Bowden <tony@tmtm.com>
2020-08-10 09:17:04 +0100

    Configure generate-qs via JSON
    Rather than needing to edit this code each time, it can now be
    configured from the master config file.

--- generate-qs.rb
+++ generate-qs.rb
@@ -2,22 +2,22 @@
 # frozen_string_literal: true
 
 require 'csv'
+require 'json'
 require 'pry'
 require 'scraped'
 
 require_relative 'lib/quickstatement_candidate'
 
-wd = 'Q48814827'
-source = 'https://en.wikipedia.org/wiki/2018_West_Tyrone_by-election'
-csvfile = 'combo.csv'
-new_person_description = 'UK election candidate'
-
-csv = CSV.table(csvfile)
+json_file = ARGV.first
+json = JSON.parse(File.read(json_file), symbolize_names: true)
+csv = CSV.table(json[:combofile])
 
 commands = csv.map do |row|
   data = row.to_h
   data[:id] ||= data.delete(:foundid)
-  QuickStatement::Candidate.new(data.merge(election: wd, url: source, description: new_person_description)).to_s
+  QuickStatement::Candidate.new(
+    data.merge(election: json[:wikidata], url: json[:wikipedia][:url], description: json[:new_person_description])
+  ).to_s
 end
 
 puts commands.join("\n")
